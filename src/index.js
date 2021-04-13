672752c4f800f5e9baea275b1f5504e3ee715295
ralzinov <azimut.nt@gmail.com>
2021-04-13 23:42:28 +0300

    Added colors to phrases

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import 'regenerator-runtime/runtime';
 import { sentence } from 'txtgen';
 import { Dogeify } from './dogeify';
+import colors from 'css-color-names';
 
 const DOGE_WIDTH = 500;
 const DOGE_HEIGHT = 500;
+const COLORS = Object.entries(colors);
 const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-function debounce(f, ms) {
+const debounce = (f, ms) => {
     let isCooldown = false;
     return function() {
         if (isCooldown) return;
@@ -14,20 +16,21 @@ function debounce(f, ms) {
         isCooldown = true;
         setTimeout(() => isCooldown = false, ms);
     };
-}
+};
 
 const placePhrases = () => {
     const {width, height} = document.body.getClientRects()[0];
     const amount = width > 900 ? 4 : 2;
     Promise.all(Array.from({length: amount}).map(() => new Dogeify().init(sentence())))
         .then((sentencies) => {
-            const container = document.createDocumentFragment()
+            const container = document.createDocumentFragment();
             sentencies.flatMap((text) => text.split('. '))
                 .forEach((phrase) => {
                     const phraseNode = document.createElement('span');
                     phraseNode.className = 'phrase';
                     phraseNode.style.left = `${rand(0, width - 50)}px`
                     phraseNode.style.top = `${rand(0, height - 50)}px`;
+                    phraseNode.style.color = COLORS[rand(0, COLORS.length)]?.[1];
                     phraseNode.innerText = phrase;
                     container.appendChild(phraseNode);
                 });
@@ -59,6 +62,5 @@ const init = () => {
     placePhrases();
 }
 
-window.addEventListener('click', init);
 window.addEventListener('DOMContentLoaded', init);
 window.addEventListener('resize', debounce(init, 1000));
